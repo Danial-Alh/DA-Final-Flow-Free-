c7bf86801c9a04564d37634b482ffa21f86d182e
Danial <d_alihosseiny@yahoo.com>
2015-06-27 15:02:02 +0430

    utilities completed 90%

--- src/utilities/PairPoint.java
+++ src/utilities/PairPoint.java
@@ -1,14 +1,16 @@
 package utilities;
 
+import java.awt.*;
+
 /**
  * Created by Dani on 6/27/2015.
  */
 public class PairPoint
 {
-    private int origin;
-    private int dest;
+    private Point origin;
+    private Point dest;
 
-    public PairPoint(int origin, int dest)
+    public PairPoint(Point origin, Point dest)
     {
         this.origin = origin;
         this.dest = dest;
