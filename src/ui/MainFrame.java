65013bb90b7438e0ab25be0001f34c9e6156e2a7
Hosein <hoseinakbo@gmail.com>
2015-06-27 14:37:43 +0430

    Built MainFrame 1%

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -1,20 +1,26 @@
 package ui;
 
 import javax.swing.*;
-import java.lang.String;
+import java.awt.*;
 
 /**
  * Created by Dani on 6/27/2015.
  */
 public class MainFrame extends JFrame
 {
-    public MainFrame()
-    {
-        setVisible(true);
-    }
+    public MainFrame(String arg0, int w, int h) {
+        super(arg0);
+        Dimension d = getToolkit().getScreenSize();
+        setSize(w,h);
+        setLocation((d.width-w)/2,(d.height-h)/2);
+        setLayout(null);
 
-    public static void main( String[] args )
-    {
-        new MainFrame();
+        getContentPane().setBackground(Color.GRAY);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+    public static void main(String[] arg){
+        new MainFrame("Flow Free", 800, 650);
     }
 }
